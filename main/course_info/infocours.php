7c9fc1665d0735d3594286b3076a894e9bd268f1
Anibal Copitan <acopitan@temari>
2014-06-24 17:16:19 -0500

    path change global inc

--- main/course_info/infocours.php
+++ main/course_info/infocours.php
@@ -12,12 +12,12 @@
  */
 
 /*	   INIT SECTION */
-
+//require_once __DIR__.'/../../vendor/autoload.php';
 use \ChamiloSession as Session;
 
 // Language files that need to be included
 $language_file = array('create_course', 'course_info', 'admin', 'gradebook', 'document');
-require_once '../inc/global.inc.php';
+require_once '../../inc/global.inc.php';
 $current_course_tool  = TOOL_COURSE_SETTING;
 $this_section = SECTION_COURSES;
 
